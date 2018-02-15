607f972a49d13fd1c96a260d0a2860144d9c9d9e
sasiroot <sasiroot@NA106-US09.ikaira.iurretalhi.eus>
2018-02-15 09:26:26 +0100

    Add OK

--- src/SecondaryDialog.java
+++ src/SecondaryDialog.java
@@ -35,6 +35,11 @@ public class SecondaryDialog extends javax.swing.JDialog {
         jLabel1.setText("Hello World!");
 
         jButton1.setText("OK");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -61,6 +66,10 @@ public class SecondaryDialog extends javax.swing.JDialog {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
